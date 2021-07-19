100e9e9a6c04f4bfb2e7384ce35ebf92081fc8d8
Cloud User <centos@worsica-testbed.novalocal>
2021-07-19 18:27:03 +0000

    kompose must be run on worsica-kubernetes repository

--- worsica_update_scrpt.sh
+++ worsica_update_scrpt.sh
@@ -16,6 +16,25 @@ echo $NO_CACHE_FLAG
 
 echo '------------------------------------'
 cd $CURRENT_PATH
+if ([[ -z $WORSICA_COMPONENT ]] || [[ $WORSICA_COMPONENT == 'kubernetes' ]]); then
+	echo ' ==========Update worsica-kubernetes  =========='
+	echo '1) git pull --------------'
+	if (cd $CURRENT_PATH && git pull origin $CURRENT_BRANCH); then
+		echo 'git pull success! --------------'
+		cd $CURRENT_PATH
+		echo '2) kompose --------------'
+		if (kompose convert --controller "deployment" -f ./backend/backend.yml); then
+			echo 'kompose success! --------------'
+			cd $CURRENT_PATH
+		else
+			echo 'kompose fail! --------------'
+			cd $CURRENT_PATH
+		fi
+	else
+		echo 'git pull fail! --------------'
+		cd $CURRENT_PATH
+	fi
+fi
 if ([[ -z $WORSICA_COMPONENT ]] || [[ $WORSICA_COMPONENT == 'essentials' ]]); then
 	echo ' ==========Update worsica-essentials  =========='
 	echo '1) git pull --------------'
