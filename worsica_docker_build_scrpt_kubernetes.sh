9f2a28c31c2f21face6288cce31c0b4b1f8b4997
Cloud User <centos@worsica-testbed.novalocal>
2021-07-19 11:34:04 +0000

    dockers were revised, now depend of the original ones. changes to the bash script

--- worsica_docker_build_scrpt_kubernetes.sh
+++ worsica_docker_build_scrpt_kubernetes.sh
@@ -1,5 +1,4 @@
 CURRENT_PATH=$HOME/worsica/worsica-kubernetes
-#$(pwd)
 
 if [[ $EUID -ne 0 ]] ; then
 	echo "Uh-uh, no way! This script must be run as root."
@@ -7,31 +6,31 @@ if [[ $EUID -ne 0 ]] ; then
 fi
 
 echo '------------------------------------'
-echo '1) Building worsica-kubernetes-essentials'
-cd $CURRENT_PATH/worsica-cicd/docker_essentials/aio_v4 
+echo '1) Building worsica-essentials'
+cd $CURRENT_PATH/repositories/worsica-cicd/docker_essentials/aio_v4 
 if [[ -z $1 ]] ; then
 	echo 'Use cache on the build'
-	if docker build -t worsica/worsica-kubernetes-essentials:development -f Dockerfile.kubernetes.essentials . ; then 
-		echo 'Building worsica-kubernetes-essentials: Success!'
+	if docker build -t worsica/worsica-essentials:development -f Dockerfile.essentials . ; then 
+		echo 'Building worsica-essentials: Success!'
 		cd $CURRENT_PATH
 	else
-		echo 'Building worsica-kubernetes-essentials: Fail!'
+		echo 'Building worsica-essentials: Fail!'
 		cd $CURRENT_PATH
 		exit 1
 	fi
 else
 	if [[ $1 == '--no-cache' ]]; then
 		echo 'Do not use cache on the build, rebuild'
-		if docker build --no-cache -t worsica/worsica-kubernetes-essentials:development -f Dockerfile.kubernetes.essentials . ; then
-			echo 'Building worsica-kubernetes-essentials: Success!'
+		if docker build --no-cache -t worsica/worsica-essentials:development -f Dockerfile.essentials . ; then
+			echo 'Building worsica-essentials: Success!'
 			cd $CURRENT_PATH
 		else
-			echo 'Building worsica-kubernetes-essentials: Fail!'
+			echo 'Building worsica-essentials: Fail!'
 			cd $CURRENT_PATH
 			exit 1
 		fi
 	else
-		echo 'Building worsica-kubernetes-essentials: Fail! Invalid argument '$1
+		echo 'Building worsica-essentials: Fail! Invalid argument '$1
 		cd $CURRENT_PATH
 		exit 1
 	fi
@@ -40,6 +39,15 @@ fi
 
 echo '------------------------------------'
 echo '2) Building worsica-kubernetes-frontend'
+cd $CURRENT_PATH/repositories/worsica-frontend/docker_frontend/aio_v4 
+if docker build -t worsica/worsica-frontend:development -f Dockerfile.frontend . ; then
+	echo 'Building worsica-frontend: Success!'
+	cd $CURRENT_PATH
+else
+	echo 'Building worsica-frontend: Fail!'
+	cd $CURRENT_PATH
+	exit 1
+fi
 cd $CURRENT_PATH/worsica_web/docker_frontend/aio_v4 
 if docker build -t worsica/worsica-kubernetes-frontend:development -f Dockerfile.kubernetes.frontend . ; then
 	echo 'Building worsica-kubernetes-frontend: Success!'
@@ -52,7 +60,16 @@ fi
 
 echo '------------------------------------'
 echo '3) Building worsica-kubernetes-intermediate'
-cd worsica_web_intermediate/docker_intermediate/aio_v4 
+cd $CURRENT_PATH/repositories/worsica-intermediate/docker_intermediate/aio_v4 
+if docker build -t worsica/worsica-intermediate:development -f Dockerfile.intermediate . ; then
+	echo 'Building worsica-intermediate: Success!'
+	cd $CURRENT_PATH
+else
+	echo 'Building worsica-intermediate: Fail!'
+	cd $CURRENT_PATH
+	exit 1
+fi
+cd $CURRENT_PATH/worsica_web_intermediate/docker_intermediate/aio_v4 
 if docker build -t worsica/worsica-kubernetes-intermediate:development -f Dockerfile.kubernetes.intermediate . ; then
 	echo 'Building worsica-kubernetes-intermediate: Success!'
 	cd $CURRENT_PATH
@@ -62,47 +79,27 @@ else
 	exit 1
 fi
 
-echo '------------------------------------'
-echo '4) Building worsica-kubernetes-processing'
-cd $CURRENT_PATH/worsica_web_products/docker_backend/aio_v4 
-if docker build -t worsica/worsica-kubernetes-processing:development -f Dockerfile.kubernetes.backend . ; then
-	echo 'Building worsica-kubernetes-backend: Success!'
-	cd $CURRENT_PATH
-else
-	echo 'Building worsica-kubernetes-backend: Fail!'
-	cd $CURRENT_PATH
-	exit 1
-fi
-
-# echo '------------------------------------'
-# echo '5) do docker copy'
-# echo '5.1) worsica-frontend'
-# if docker cp $HOME/worsica/worsica-frontend worsica/worsica-kubernetes-frontend:development:/usr/local/worsica_web ; then
-# 	echo 'Copying worsica-frontend to docker frontend: Success!'
-# 	cd $CURRENT_PATH
-# else
-# 	echo 'Copying worsica-frontend to docker frontend: Fail!'
-# 	cd $CURRENT_PATH
-# 	exit 1
-# fi
-# echo '5.2) worsica-intermediate'
-# if docker cp $HOME/worsica/worsica-intermediate worsica/worsica-kubernetes-intermediate:development:/usr/local/worsica_web_intermediate ; then
-# 	echo 'Copying worsica-intermediate to docker intermediate: Success!'
-# 	cd $CURRENT_PATH
-# else
-# 	echo 'Copying worsica-intermediate to docker intermediate: Fail!'
-# 	cd $CURRENT_PATH
-# 	exit 1
-# fi
-# echo '5.3) worsica-processing'
-# if docker cp $HOME/worsica/worsica-processing worsica/worsica-kubernetes-intermediate:development:/usr/local/worsica_web_products ; then
-# 	echo 'Copying worsica-processing to docker intermediate: Success!'
-# 	cd $CURRENT_PATH
-# else
-# 	echo 'Copying worsica-processing to docker intermediate: Fail!'
-# 	cd $CURRENT_PATH
-# 	exit 1
-# fi
+#not needed for now
+#echo '------------------------------------'
+#echo '4) Building worsica-kubernetes-processing'
+#cd $CURRENT_PATH/repositories/worsica-processing/docker_backend/aio_v4 
+#if docker build -t worsica/worsica-processing:development -f Dockerfile.backend . ; then
+#	echo 'Building worsica-backend: Success!'
+#	cd $CURRENT_PATH
+#else
+#	echo 'Building worsica-backend: Fail!'
+#	cd $CURRENT_PATH
+#	exit 1
+#fi
+#cd $CURRENT_PATH/worsica_web_products/docker_backend/aio_v4 
+#if docker build -t worsica/worsica-kubernetes-processing:development -f Dockerfile.kubernetes.backend . ; then
+#	echo 'Building worsica-kubernetes-backend: Success!'
+#	cd $CURRENT_PATH
+#else
+#	echo 'Building worsica-kubernetes-backend: Fail!'
+#	cd $CURRENT_PATH
+#	exit 1
+#fi
 
 #echo '------------------------------------'
 #echo '6) Stop worsica-kubernetes-frontend docker and worsica-kubernetes-intermediate docker'
